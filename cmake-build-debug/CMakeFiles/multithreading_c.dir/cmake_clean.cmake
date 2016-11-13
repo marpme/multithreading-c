f3bb519e07da3c006a278d88821645307db32f41
Marvin Piekarek <marvinpiekarek@Marvins-MBP.fritz.box>
2016-11-13 20:22:37 +0100

    merged to a real project

---
+++ cmake-build-debug/CMakeFiles/multithreading_c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/multithreading_c.dir/src/thread_bsp.c.o"
+  "multithreading_c.pdb"
+  "multithreading_c"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/multithreading_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
